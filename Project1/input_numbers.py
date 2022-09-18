0c6eedc54012cc2734877ed90999d3c43c5fa418
Alex <alexandrakgomez@arizona.edu>
2022-09-18 15:34:31 -0700

    updated loops with def ()

--- Project1/input_numbers.py
+++ Project1/input_numbers.py
@@ -1,14 +1,32 @@
-# while loop with user input
-while True:
+def user_input():
     try:
         number = int(input("Please enter an integer: "))
-        print(f"Success!")
-        break
+        print(f"Success! Number is {number}")
+        numbers_func(number)
+        question_func()
+
     except ValueError:
         print("Error - not an integer!")
-        continue
+        user_input()
 
-# This appends the user_input to the numbers.txt file
-numbers_file = open("numbers.txt", "a")
-numbers_file.write(f"\n{number}")
-numbers_file.close()
+def question_func():
+    question = input("Do you want to continue? Enter either 'Yes' or 'No'. ")
+
+    if question.lower() == "yes" or question.lower() == "y":
+        user_input()
+    elif question.lower() == "no" or question.lower() == "n":
+        print("Okay, done!")
+        return 0
+    else:
+        print("Please enter either 'Yes' or 'No'.")
+        question_func()
+
+
+def numbers_func(number):
+    # This appends the user_input to the numbers.txt file
+    numbers_file = open("numbers.txt", "a")
+    numbers_file.write(f"\n{number}")
+    numbers_file.close()
+
+
+user_input()
