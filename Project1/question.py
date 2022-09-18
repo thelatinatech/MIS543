25074fe4e5d64f83dd55c69f75779ebc6aa7e67f
Alex <alexandrakgomez@arizona.edu>
2022-09-18 10:22:27 -0700

    while loop with user input added

--- Project1/question.py
+++ Project1/question.py
@@ -1,1 +1,15 @@
-# While loop with Yes or No and user input
+# While loop with User Input
+while True:
+    yes_no = input("Do you want to continue? Yes or No?: ")
+
+    if yes_no.lower() == 'yes' or yes_no.lower() == 'y':
+        print("Okay!")
+        continue
+
+    elif yes_no.lower() == 'no' or yes_no.lower() == 'n':
+        print("Done!")
+        break
+
+    else:
+        print("Type either 'yes' or 'no'. ")
+        continue
